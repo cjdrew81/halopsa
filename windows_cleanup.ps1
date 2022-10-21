928f609e8663f7abd25e9f4bc10fc20b2386c0d2
Chris Drew <46671472+cjdrew81@users.noreply.github.com>
2022-10-21 18:53:38 +0100

    Update windows_cleanup.ps1

--- windows_cleanup.ps1
+++ windows_cleanup.ps1
@@ -2,6 +2,12 @@ param(
 [string]$TicketID
 )
 
+$FolderTest = Test-Path 'c:\script'
+If (!$FolderTest){
+New-Item -Path c:\ -Name Script -ItemType Directory -Force
+}
+
+
 $SpaceReport = @()
 $FreespaceBefore = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
 $SpaceReport += "Free Space at start of script - $($FreeSpaceBefore) GB"
@@ -140,24 +146,6 @@ $SpaceReport += "Deleted $($OST)"
 $FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
 $SpaceReport += "Free space after removing old OST files - $($Freespace) GB"
 
-
-
-
-
-
-$EmailHeader = @"
-<style>
-TABLE {width: auto; height: auto; border-width: 1px; margin: auto; border-style: solid; border-color: black; border-collapse: collapse;}
-TH {color: white; width:auto;border-width: 1px; padding: 2px; border-style: solid; border-color: black; background-color: #6495ED;text-align:left;}
-TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;text-align:left;font-size:10px;}
-IMG {display: block; width: 30%; height: 30%; text-align: center;}
-.logo {display: block; width: 40%; height: 40%; text-align: center;}
-</style>
-<img src="https://cloud10.it/images/cloud10_reports.png" Width="300" Height="70">
-<h1>Disk Report - $($ENV:COMPUTERNAME)</h1>
-"@
-
-
 $fileslimit = 30
 $filesLocation = 'C:\'
 $largeSizefiles = get-ChildItem -path $filesLocation -recurse -ErrorAction "SilentlyContinue" | ? { $_.GetType().Name -eq "FileInfo" } | where-Object {$_.Length -gt $fileSize} | sort-Object -property length -Descending | Select-Object Name, @{Name="Size In MB";Expression={ "{0:N0}" -f ($_.Length / 1MB)}},@{Name="LastWriteTime";Expression={$_.LastWriteTime}},@{Name="Path";Expression={$_.directory}} -first $filesLimit
@@ -166,26 +154,7 @@ $Report = $largeSizefiles | convertto-html -head $EmailHeader
 $FreespaceReport = $SpaceReport | select @{L = "Task"; E = { ($_.split("-"))[0] } }, @{L = "Free Space" ; E = { ($_.split("-"))[1]}} | convertto-html -Fragment
 $FreespaceReport | out-file 'c:\windows\temp\freespacereport.txt'
 
-$DiskInfo = Get-PhysicalDisk | Select mediatype,friendlyname,operationalstatus,healthstatus,@{L = "Size"; E = {[math]::round($_.size / 1GB,2)}} | convertto-html
+$DiskInfo = Get-PhysicalDisk | Select mediatype,friendlyname,operationalstatus,healthstatus,@{L = "Size"; E = {[math]::round($_.size / 1GB,2)}} | convertto-html | 
 
 
-#Add-Type -AssemblyName System.Web
-#$Body = ([System.Web.HttpUtility]::HtmlDecode($HTMLContent))
 
-#$File = $Feedbacks | ? updated_at -GT $range | Convertto-CSV | out-file $OutFilePath -encoding UTF8 -append
-#$attachment = new-object Net.Mail.Attachment("$OutFilePath")
-$fromaddress = 'reports@cloud10.it'
-$toaddress = 'support@cloud10.it'
-$Subject = "Hard Drive Report - $($ENV:COMPUTERNAME) - Update Ticket [ID:00$($TicketID)] "
-$port = '25'
-$message = New-Object System.Net.Mail.MailMessage
-$message.IsBodyHTML = $True
-$message.From = $fromaddress
-$message.To.Add($toaddress)
-$message.Subject = $Subject
-#$message.Attachments.Add($attachment)
-$message.body = $Report + $FreeSpaceReport + $DiskInfo
-$SMTP = "cloud10-it.mail.protection.outlook.com"
-$smtp = New-Object Net.Mail.SmtpClient($smtp, $port)
-$smtp.EnableSsl = $true
-$smtp.Send($message)
