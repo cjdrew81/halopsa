1ef12d3b2880228fb27b1ea36dd352173dad6526
Chris Drew <46671472+cjdrew81@users.noreply.github.com>
2022-10-23 00:51:06 +0100

    Update windows_cleanup.ps1

--- windows_cleanup.ps1
+++ windows_cleanup.ps1
@@ -147,7 +147,7 @@ $SpaceReport += "Deleted $($OST)"
 $FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
 $SpaceReport += "Free space after removing old OST files - $($Freespace) GB"
 
-$fileslimit = 30
+$fileslimit = 20
 $filesLocation = 'C:\'
 $largeSizefiles = get-ChildItem -path $filesLocation -recurse -ErrorAction "SilentlyContinue" | ? { $_.GetType().Name -eq "FileInfo" } | where-Object {$_.Length -gt $fileSize} | sort-Object -property length -Descending | Select-Object Name, @{Name="Size In MB";Expression={ "{0:N0}" -f ($_.Length / 1MB)}},@{Name="LastWriteTime";Expression={$_.LastWriteTime}},@{Name="Path";Expression={$_.directory}} -first $filesLimit
 $Report = $largeSizefiles | convertto-html | out-file 'c:\scripts\filelist.html'
