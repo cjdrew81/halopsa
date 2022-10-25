12f40595eb903722e9fb6bf3b03ad766d9601be8
Chris Drew <cjdrew81@gmail.com>
2022-10-25 01:51:34 +0100

    Removed Cleanmgr section

--- windows_cleanup.ps1
+++ windows_cleanup.ps1
@@ -1,11 +1,11 @@
 $FolderTest = Test-Path 'c:\scripts'
-If (!$FolderTest){
-New-Item -Path c:\ -Name Scripts -ItemType Directory -Force
+If (!$FolderTest) {
+    New-Item -Path c:\ -Name Scripts -ItemType Directory -Force
 }
 
 
 $SpaceReport = @()
-$FreespaceBefore = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
+$FreespaceBefore = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace / 1GB), 2)
 $SpaceReport += "Free Space at start of script - $($FreeSpaceBefore) GB"
 
 # This will empty the recycle bin
@@ -13,36 +13,36 @@ $SpaceReport += "Free Space at start of script - $($FreeSpaceBefore) GB"
 $Date = Get-Date -Format 'dd-MM-yy'
 $objShell = New-Object -ComObject Shell.Application
 $objFolder = $objShell.Namespace(0xA)
-$objFolder.items() | %{ remove-item $_.path -Recurse -Confirm:$true}
+$objFolder.items() | % { remove-item $_.path -Recurse -Confirm:$true }
 
-$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
+$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace / 1GB), 2)
 $SpaceReport += "Free Space after Empty Recycle Bin - $($FreeSpace) GB" 
 
 $Hiber = test-path 'C:\hiber.sys'
-If ($Hiber){
-powercfg /H off
-$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
-$SpaceReport += "Free Space after disabling hibernation - $($FreeSpace) GB"
+If ($Hiber) {
+    powercfg /H off
+    $FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace / 1GB), 2)
+    $SpaceReport += "Free Space after disabling hibernation - $($FreeSpace) GB"
 }
 
 if (test-path 'C:\Config.Msi') {
-remove-item -Path 'C:\Config.Msi' -force -recurse
+    remove-item -Path 'C:\Config.Msi' -force -recurse
 }
 if (test-path 'c:\Intel') {
-remove-item -Path 'c:\Intel' -force -recurse
+    remove-item -Path 'c:\Intel' -force -recurse
 }
-if (test-path 'c:\PerfLogs'){
-remove-item -Path 'c:\PerfLogs' -force -recurse
+if (test-path 'c:\PerfLogs') {
+    remove-item -Path 'c:\PerfLogs' -force -recurse
 }
 if (test-path 'c:\swsetup') {
-remove-item -Path 'c:\swsetup' -force -recurse
+    remove-item -Path 'c:\swsetup' -force -recurse
 }
 if (test-path '$env:windir\memory.dmp') {
-remove-item '$env:windir\memory.dmp' -force
+    remove-item '$env:windir\memory.dmp' -force
 }
 
 # Deleting Windows Error Reporting files
-if (test-path 'C:\ProgramData\Microsoft\Windows\WER'){Get-ChildItem -Path C:\ProgramData\Microsoft\Windows\WER -Recurse | Remove-Item -force -recurse}
+if (test-path 'C:\ProgramData\Microsoft\Windows\WER') { Get-ChildItem -Path C:\ProgramData\Microsoft\Windows\WER -Recurse | Remove-Item -force -recurse }
 
 
 
@@ -59,7 +59,7 @@ Remove-Item -Path "C:\Users\*\AppData\Local\Microsoft\Windows\IEDownloadHistory\
 Remove-Item -Path "C:\Users\*\AppData\Local\Microsoft\Windows\INetCache\*" -Force -Recurse
 Remove-Item -Path "C:\Users\*\AppData\Local\Microsoft\Windows\INetCookies\*" -Force -Recurse
 Remove-Item -Path "C:\Users\*\AppData\Local\Microsoft\Terminal Server Client\Cache\*" -Force -Recurse
-$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
+$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace / 1GB), 2)
 $SpaceReport += "Free space after removing system and user temp files - $($FreeSpace) GB"
 
 
@@ -70,9 +70,10 @@ Remove-Item -Path "$env:windir\SoftwareDistribution\*" -Force -Recurse
 Remove-Item $env:windir\Logs\CBS\* -force -recurse
 Start-Service wuauserv -Verbose
 Start-Service TrustedInstaller -Verbose
-$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
+$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace / 1GB), 2)
 $SpaceReport += "Free Space after removing WIndows Updates cache - $($FreeSpace) GB"
 
+<#
 Write-host "Checkif Windows Cleanup exists" -foreground yellow
 #Mainly for 2008 servers
 if (!(Test-Path c:\windows\System32\cleanmgr.exe)) {
@@ -118,42 +119,45 @@ if  (-not (get-itemproperty -path 'HKLM:\Software\Microsoft\Windows\CurrentVersi
 }
 
 Start-Process -FilePath CleanMgr.exe -ArgumentList $StateRun  -WindowStyle Hidden -Wait
-$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
+
+#>
+
+$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace / 1GB), 2)
 $SPaceReport += "Free space after running disk cleanup tool - $($FreeSpace) GB"
 
-wevtutil el | Foreach-Object {Write-Host "Clearing $_"; wevtutil cl "$_"}
+wevtutil el | Foreach-Object { Write-Host "Clearing $_"; wevtutil cl "$_" }
 
 $OSTList = @()
 $users = Get-ChildItem 'c:\users'
-foreach ($u in $users){
-$folder = 'C:\users\' + $u.name +"\appdata\local\microsoft"
-$folderpath = test-path -Path $folder
-if($folderpath)
-{
-$OSTList += (Get-ChildItem $Folder -filter "*.ost" -Recurse -Force | where-object {($_.LastWriteTime -lt (Get-Date).AddDays(-60))}).fullname
+foreach ($u in $users) {
+    $folder = 'C:\users\' + $u.name + "\appdata\local\microsoft"
+    $folderpath = test-path -Path $folder
+    if ($folderpath) {
+        $OSTList += (Get-ChildItem $Folder -filter "*.ost" -Recurse -Force | where-object { ($_.LastWriteTime -lt (Get-Date).AddDays(-60)) }).fullname
+    }
 }
+If ($OSTList) {
+    Foreach ($OST in $OSTList) {
+        if ($ost -like "*.ost*") { 
+            Remove-Item -Path $OST -Force
+            $SpaceReport += "Deleted $($OST)"
+        }
+    }
 }
-If ($OSTList){
-Foreach ($OST in $OSTList){
-if ($ost -like "*.ost*"){ 
-Remove-Item -Path $OST -Force
-$SpaceReport += "Deleted $($OST)"
-}
-}
-} else {$SpaceReport += "No OST files found to remove"}
+else { $SpaceReport += "No OST files found to remove" }
 
-$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace/1GB),2)
+$FreeSpace = [math]::Round(((Get-WmiObject win32_logicaldisk -filter "DeviceID='C:'" | select Freespace).FreeSpace / 1GB), 2)
 $SpaceReport += "Free space after removing old OST files - $($Freespace) GB"
 
 $fileslimit = 20
 $filesLocation = 'C:\'
-$largeSizefiles = get-ChildItem -path $filesLocation -recurse -ErrorAction "SilentlyContinue" | ? { $_.GetType().Name -eq "FileInfo" } | where-Object {$_.Length -gt $fileSize} | sort-Object -property length -Descending | Select-Object Name, @{Name="Size In MB";Expression={ "{0:N0}" -f ($_.Length / 1MB)}},@{Name="LastWriteTime";Expression={$_.LastWriteTime}},@{Name="Path";Expression={$_.directory}} -first $filesLimit
+$largeSizefiles = get-ChildItem -path $filesLocation -recurse -ErrorAction "SilentlyContinue" | ? { $_.GetType().Name -eq "FileInfo" } | where-Object { $_.Length -gt $fileSize } | sort-Object -property length -Descending | Select-Object Name, @{Name = "Size In MB"; Expression = { "{0:N0}" -f ($_.Length / 1MB) } }, @{Name = "LastWriteTime"; Expression = { $_.LastWriteTime } }, @{Name = "Path"; Expression = { $_.directory } } -first $filesLimit
 $Report = $largeSizefiles | convertto-html -fragment | out-file 'c:\scripts\filelist.html'
 
-$FreespaceReport = $SpaceReport | select @{L = "Task"; E = { ($_.split("-"))[0] } }, @{L = "Free Space" ; E = { ($_.split("-"))[1]}} | convertto-html -Fragment
+$FreespaceReport = $SpaceReport | select @{L = "Task"; E = { ($_.split("-"))[0] } }, @{L = "Free Space" ; E = { ($_.split("-"))[1] } } | convertto-html -Fragment
 $FreespaceReport | out-file 'c:\scripts\freespacereport.txt'
 
-$DiskInfo = Get-PhysicalDisk | Select mediatype,friendlyname,operationalstatus,healthstatus,@{L = "Size"; E = {[math]::round($_.size / 1GB,2)}} | convertto-html | out-file 'c:\scripts\drives.html'
+$DiskInfo = Get-PhysicalDisk | Select mediatype, friendlyname, operationalstatus, healthstatus, @{L = "Size"; E = { [math]::round($_.size / 1GB, 2) } } | convertto-html | out-file 'c:\scripts\drives.html'
 $FreeSpaceReport
 
 
